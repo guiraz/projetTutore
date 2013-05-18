b5dbfe4ac58097adc03b8cd7d1c81fb4795ba87f
RAZIMBAUD Guillaume <grazimbaud@gmail.com>
2013-05-19 00:05:30 +0300

    Update main.cpp for windows

--- dislec-clik/source/main.cpp
+++ dislec-clik/source/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    //QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
 
     Controller c;
     c.show();
